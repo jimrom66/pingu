246d1927f864139d40e85cdd8a46d1426c0f7cef
jimrom66 <188324624+jimrom66@users.noreply.github.com>
2025-02-21 13:13:55 +0200

    finished the memory storage, now to make the graphs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const entriesManager={
 
     entriesLog:[],
@@ -6,7 +8,7 @@ const entriesManager={
     
     getCurrentTime:function(){
         const currenttime= new Date;
-        return currenttime.toLocaleTimeString();
+        return currenttime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     },
 
     customerEntry: function(numberOfPeople=1) {
@@ -14,12 +16,14 @@ const entriesManager={
         const time = this.getCurrentTime();
         this.entriesLog.push({people, time });
         this.customerNow.push({people})
+        memoryManager.saveToLocalStorage(this.entriesLog,this.customerNow);
         
         
     },
     customerLeft:function(){
         if (this.customerNow.length >0){
             this.customerNow.pop();
+            memoryManager.saveToLocalStorage(this.entriesLog,this.customerNow);
         }else return;
     },
     findAllEntries:function(){
@@ -31,7 +35,12 @@ const entriesManager={
             sum+=entry.people;
         })
         return sum;
+    },
+    restartArrays: function(){
+        this.entriesLog=[];
+        this.customerNow=[];
     }
+
     
 }
 
@@ -50,10 +59,12 @@ const renderManager={
 const clickHandler={
     plusButton: document.querySelector(".add-button"),
     minusButton:document.querySelector(".minus-button"),
+    restartButton:document.querySelector(".restart"),
 
     init:function(){
         this.plusButton.addEventListener("click",()=>this.plusClick());
         this.minusButton.addEventListener("click",()=>this.minusClick());
+        this.restartButton.addEventListener("click",()=>this.restartClick());
     },
 
     plusClick:function(){
@@ -65,9 +76,49 @@ const clickHandler={
     minusClick:function(){
         entriesManager.customerLeft();
         renderManager.update();
+    },
+    restartClick:function(){
+        memoryManager.clearCache();
+        entriesManager.restartArrays();
+        renderManager.update()
     }
 
 
 
 }
-clickHandler.init()
+
+
+const memoryManager={
+    
+    clearCache:function(){
+        localStorage.clear()
+    },
+
+    saveToLocalStorage: function (entriesLog, customerNow) {
+        
+        localStorage.setItem('entriesLog', JSON.stringify(entriesLog));
+        localStorage.setItem('customerNow', JSON.stringify(customerNow));
+    },
+
+    loadFromLocalStorage: function () {
+        
+        const entriesLogData = JSON.parse(localStorage.getItem('entriesLog'));
+        const customerNowData = JSON.parse(localStorage.getItem('customerNow'));
+
+        // Return the data if available, else return empty arrays
+        return {
+            entriesLog: entriesLogData || [],
+            customerNow: customerNowData || []
+        };
+        
+    },
+
+    loadDataToManager: function (entriesManager) {
+        const data = this.loadFromLocalStorage();
+        entriesManager.entriesLog = data.entriesLog;
+        entriesManager.customerNow = data.customerNow;
+    }
+}
+clickHandler.init();
+memoryManager.loadDataToManager(entriesManager);
+renderManager.update();
